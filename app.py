e83724feddec6ebc76c2f5982803c230a5d91c4e
AYUSH SHARMA <aayushparashar984@gmail.com>
2024-05-27 17:43:55 +0530

    update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 import pickle
 import streamlit as st
-import lib
+
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 import requests
 st.set_page_config(layout="wide", page_title="AQI Predictor")
 import warnings
